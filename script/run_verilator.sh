d74d029fcf3f4e68cfdbbe57922385c8158b7c2b
Akifumi Fujita <fujita.akifumi@gmail.com>
2018-10-20 10:03:10 +0900

    move verilator work dir

--- script/run_verilator.sh
+++ script/run_verilator.sh
@@ -2,8 +2,24 @@
 
 # Move to project top directory
 pushd `dirname $0`
-cd ../Modules/Timer
+cd ..
 
-verilator --cc Timer.sv
+mkdir -p work/verilator
+
+verilator -Mdir work/verilator/test_Core --cc \
+    Modules/Packages/BasicTypes.sv \
+    Modules/Packages/Rv32Types.sv \
+    Modules/Packages/RvTypes.sv \
+    Modules/Tlb/Packages/TlbTypes.sv \
+    Processor/Packages/ProcessorTypes.sv \
+    Processor/Packages/CacheTypes.sv \
+    Processor/Packages/MemoryTypes.sv \
+    Processor/Packages/OpTypes.sv \
+    Processor/Packages/TraceTypes.sv \
+    Processor/Sources/Decoder/Packages/Decoder.sv \
+    Processor/Sources/LoadStoreUnit/Packages/LoadStoreUnitTypes.sv
+
+verilator -Mdir work/verilator/test_Timer --cc \
+    Modules/Timer/Timer.sv
 
 popd
