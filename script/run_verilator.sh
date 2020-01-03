4e1eec58677ea102489c255e0197b40af52e299c
Akifumi Fujita <fujita.akifumi@gmail.com>
2020-01-03 10:20:05 +0900

    fix run_verilator.sh

--- script/run_verilator.sh
+++ script/run_verilator.sh
@@ -7,58 +7,58 @@ cd ..
 mkdir -p work/verilator
 
 verilator -Mdir work/verilator/test_Core --cc --trace --top-module Core \
-    module/package/BasicTypes.sv \
-    module/package/Rv32Types.sv \
-    module/package/RvTypes.sv \
-    module/Tlb/package/TlbTypes.sv \
-    cpu/package/ProcessorTypes.sv \
-    cpu/package/CacheTypes.sv \
-    cpu/package/MemoryTypes.sv \
-    cpu/package/OpTypes.sv \
-    cpu/src/Decoder/package/Decoder.sv \
-    cpu/src/LoadStoreUnit/package/LoadStoreUnitTypes.sv \
-    module/DivUnit/src/DivUnit.sv \
-    module/MulUnit/src/MulUnit.sv \
-    module/Memory/BlockRam.sv \
-    module/Memory/FlipFlopCam.sv \
-    module/Tlb/Tlb.sv \
-    module/Tlb/TlbReplacer.sv \
-    module/Reset/ResetSequencer.sv \
-    cpu/src/FetchUnit/FetchUnit.sv \
-    cpu/src/FetchUnit/FetchUnitIF.sv \
-    cpu/src/FetchUnit/ICacheInvalidater.sv \
-    cpu/src/FetchUnit/ICacheReplacer.sv \
-    cpu/src/LoadStoreUnit/DCacheReplacer.sv \
-    cpu/src/LoadStoreUnit/LoadStoreUnitIF.sv \
-    cpu/src/LoadStoreUnit/LoadStoreUnit.sv \
-    cpu/src/BusAccessUnit/BusAccessUnit.sv \
-    cpu/src/BusAccessUnit/BusAccessUnitIF.sv \
-    cpu/src/BypassLogic.sv \
-    cpu/src/BypassLogicIF.sv \
-    cpu/src/ControlStatusRegister.sv \
-    cpu/src/ControlStatusRegisterIF.sv \
-    cpu/src/DecodeStage.sv \
-    cpu/src/DecodeStageIF.sv \
-    cpu/src/ExecuteStage.sv \
-    cpu/src/ExecuteStageIF.sv \
-    cpu/src/FetchStage.sv \
-    cpu/src/FetchStageIF.sv \
-    cpu/src/MemoryAccessStage.sv \
-    cpu/src/MemoryAccessStageIF.sv \
-    cpu/src/PipelineController.sv \
-    cpu/src/PipelineControllerIF.sv \
-    cpu/src/RegFile.sv \
-    cpu/src/RegFileIF.sv \
-    cpu/src/RegReadStage.sv \
-    cpu/src/RegReadStageIF.sv \
-    cpu/src/RegWriteStage.sv \
-    cpu/src/Core.sv
+    src/hw/module/package/BasicTypes.sv \
+    src/hw/module/package/Rv32Types.sv \
+    src/hw/module/package/RvTypes.sv \
+    src/hw/module/Tlb/package/TlbTypes.sv \
+    src/hw/cpu/package/ProcessorTypes.sv \
+    src/hw/cpu/package/CacheTypes.sv \
+    src/hw/cpu/package/MemoryTypes.sv \
+    src/hw/cpu/package/OpTypes.sv \
+    src/hw/cpu/src/Decoder/package/Decoder.sv \
+    src/hw/cpu/src/LoadStoreUnit/package/LoadStoreUnitTypes.sv \
+    src/hw/module/DivUnit/src/DivUnit.sv \
+    src/hw/module/MulUnit/src/MulUnit.sv \
+    src/hw/module/Memory/BlockRam.sv \
+    src/hw/module/Memory/FlipFlopCam.sv \
+    src/hw/module/Tlb/Tlb.sv \
+    src/hw/module/Tlb/TlbReplacer.sv \
+    src/hw/module/Reset/ResetSequencer.sv \
+    src/hw/cpu/src/FetchUnit/FetchUnit.sv \
+    src/hw/cpu/src/FetchUnit/FetchUnitIF.sv \
+    src/hw/cpu/src/FetchUnit/ICacheInvalidater.sv \
+    src/hw/cpu/src/FetchUnit/ICacheReplacer.sv \
+    src/hw/cpu/src/LoadStoreUnit/DCacheReplacer.sv \
+    src/hw/cpu/src/LoadStoreUnit/LoadStoreUnitIF.sv \
+    src/hw/cpu/src/LoadStoreUnit/LoadStoreUnit.sv \
+    src/hw/cpu/src/BusAccessUnit/BusAccessUnit.sv \
+    src/hw/cpu/src/BusAccessUnit/BusAccessUnitIF.sv \
+    src/hw/cpu/src/BypassLogic.sv \
+    src/hw/cpu/src/BypassLogicIF.sv \
+    src/hw/cpu/src/ControlStatusRegister.sv \
+    src/hw/cpu/src/ControlStatusRegisterIF.sv \
+    src/hw/cpu/src/DecodeStage.sv \
+    src/hw/cpu/src/DecodeStageIF.sv \
+    src/hw/cpu/src/ExecuteStage.sv \
+    src/hw/cpu/src/ExecuteStageIF.sv \
+    src/hw/cpu/src/FetchStage.sv \
+    src/hw/cpu/src/FetchStageIF.sv \
+    src/hw/cpu/src/MemoryAccessStage.sv \
+    src/hw/cpu/src/MemoryAccessStageIF.sv \
+    src/hw/cpu/src/PipelineController.sv \
+    src/hw/cpu/src/PipelineControllerIF.sv \
+    src/hw/cpu/src/RegFile.sv \
+    src/hw/cpu/src/RegFileIF.sv \
+    src/hw/cpu/src/RegReadStage.sv \
+    src/hw/cpu/src/RegReadStageIF.sv \
+    src/hw/cpu/src/RegWriteStage.sv \
+    src/hw/cpu/src/Core.sv
 
 verilator -Mdir work/verilator/test_DivUnit --cc --trace --top-module DivUnit32 \
-    module/DivUnit/src/DivUnit.sv \
-    module/DivUnit/src/DivUnit32.sv
+    src/hw/module/DivUnit/src/DivUnit.sv \
+    src/hw/module/DivUnit/src/DivUnit32.sv
 
 verilator -Mdir work/verilator/test_Timer --cc  --trace --top-module Timer \
-    module/Timer/Timer.sv
+    src/hw/module/Timer/Timer.sv
 
 popd
