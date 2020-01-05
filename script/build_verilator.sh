cfffac4a42ed4dd976aaa07b0a8f7c5c844ab6b9
Akifumi Fujita <fujita.akifumi@gmail.com>
2020-01-05 11:54:47 +0900

    add script to build verilator

---
+++ script/build_verilator.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+exit_code="1"
+
+# Move to project top directory
+pushd `dirname $0`
+source ./common.sh.inc
+cd ../third_party/verilator
+
+export VERILATOR_ROOT=`pwd`
+
+autoconf
+./configure
+make -j${RAFI_NUM_CPU}
+exit_code="$?"
+
+popd
+
+exit ${exit_code}
