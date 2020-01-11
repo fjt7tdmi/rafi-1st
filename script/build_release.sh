b07d7020f567945cbdf092b0c10cbb8073e5bb7b
Akifumi Fujita <fujita.akifumi@gmail.com>
2020-01-11 21:34:11 +0900

    use VS2019

--- script/build_release.sh
+++ script/build_release.sh
@@ -13,7 +13,7 @@ mkdir -p build_Release
 cd build_Release
 
 if [[ -v RAFI_WIN ]]; then
-    cmake .. -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 15 2017 Win64" && cmake --build . --config Release
+    cmake .. -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 16 2019" -A x64 && cmake --build . --config Release
     exit_code="$?"
 else
     cmake .. -DCMAKE_BUILD_TYPE=Release -G Ninja && cmake --build . --config Release
